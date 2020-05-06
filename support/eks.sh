5d1c828d1d4df26891684aa48789ba34451827b2
Aruna Lakmal <arunarandunu@gmail.com>
2020-05-06 18:52:43 +0530

    Update eks.sh

--- support/eks.sh
+++ support/eks.sh
@@ -24,7 +24,7 @@ touch "${OUTPUT_PATH}"
 
 # Run the GCP/GKE profile for cloud resources
 echo -n "Generating results..."
-#export AWS_DEFAULT_REGION="${awsregion}"
+export AWS_DEFAULT_REGION=us-east-1
 cinc-auditor exec "${PROFILE_BASE_PATH}/inspec-profile-eks" -t aws:// --input awsregion="${awsregion}" clustername="${clustername}" bucketname="${bucketname}" --reporter=json:- | ./inspec-results-parser.rb >> "${OUTPUT_PATH}" || exit 1
 echo "done."
 
